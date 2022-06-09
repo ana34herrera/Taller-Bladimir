614f48989fe67b09e135dd6b3aaeb03547789c18
Camila Lozano <mclozano90@misena.edu.co>
2022-06-09 10:23:58 -0500

    empiezo a poner código en el frmLogin

--- tallerRetoBladi/Form1.Designer.cs
+++ tallerRetoBladi/Form1.Designer.cs
@@ -28,20 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnInicioSesion = new System.Windows.Forms.Button();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.BackColor = System.Drawing.Color.Red;
+            this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrar.ForeColor = System.Drawing.Color.White;
+            this.btnCerrar.Location = new System.Drawing.Point(421, -2);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(76, 47);
+            this.btnCerrar.TabIndex = 0;
+            this.btnCerrar.Text = "X";
+            this.btnCerrar.UseVisualStyleBackColor = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-1, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(426, 48);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bienvenido a McDog";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnInicioSesion
+            // 
+            this.btnInicioSesion.BackColor = System.Drawing.Color.Black;
+            this.btnInicioSesion.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnInicioSesion.ForeColor = System.Drawing.Color.White;
+            this.btnInicioSesion.Location = new System.Drawing.Point(188, 249);
+            this.btnInicioSesion.Name = "btnInicioSesion";
+            this.btnInicioSesion.Size = new System.Drawing.Size(99, 33);
+            this.btnInicioSesion.TabIndex = 2;
+            this.btnInicioSesion.Text = "Ingresar";
+            this.btnInicioSesion.UseVisualStyleBackColor = false;
+            this.btnInicioSesion.Click += new System.EventHandler(this.btnInicioSesion_Click);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(179, 113);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(124, 26);
+            this.txtUsuario.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(179, 188);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(124, 26);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(184, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Contraseña";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(199, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Usuario";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(184, 220);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(103, 18);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Ver contraseña";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // FrmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(494, 323);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.btnInicioSesion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCerrar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnInicioSesion;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- tallerRetoBladi/Form1.cs
+++ tallerRetoBladi/Form1.cs
@@ -7,14 +7,83 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace tallerRetoBladi
 {
     public partial class FrmLogin : Form
     {
+        public static string cadena = "Sever=;Database=;User Id=USENA;Password=12345;";
+        public static string msError = "";
+        public static SqlConnection conection = new SqlConnection(cadena);
+        public static DataTable dt = new DataTable();
         public FrmLogin()
         {
             InitializeComponent();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (txtPassword.PasswordChar == '*')
+            {
+                txtPassword.PasswordChar = '\0';
+            }
+            else
+            {
+                txtPassword.PasswordChar = '*';
+            }
+            txtPassword.Focus();
+        }
+
+        private void btnInicioSesion_Click(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text.Trim() == "" || txtPassword.Text.Trim() == "")
+            {
+                MessageBox.Show("ingrese los datos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                conection.Open();
+                DataTable dt = new DataTable();
+                dt = ModuloLogin.Func_TraerDatos(TxtUsuario.Text);
+                if (dt.Rows.Count == 0)
+                {
+                    MessageBox.Show("No se encontró el usuario", "¡ERROR!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    string pass = dt.Rows[0]["PASSWORD_USUARIO"].ToString();
+                    string rol = dt.Rows[0]["ROL_USUARIO"].ToString();
+                    string NOM = dt.Rows[0]["NOMBRE_USUARIO"].ToString();
+                    string APE = dt.Rows[0]["APELLIDO_USUARIO"].ToString();
+                    //comparo
+                    if (TxtContraseña.Text == pass)
+                    {
+                        FrmMenuPrincipal f = new FrmMenuPrincipal();
+                        f.Lbl_Usuario_Nombre.Text = NOM;
+                        f.Lbl_Usuario_Apellido.Text = APE;
+                        if (rol == "Administrador")
+                        {
+                            f.BtnUsuarios.Enabled = true;
+                        }
+                        else
+                        {
+                            f.BtnUsuarios.Enabled = false;
+                        }
+                        this.Hide();
+                        f.ShowDialog();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Más fácil te sabes la contraseña de tu ex :V", "¡ERROR!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
         }
     }
 }
