10e8d4860629cce9c3225f9d80c66d1cfef4891c
Camila Lozano <mclozano90@misena.edu.co>
2022-06-09 10:47:49 -0500

    creo la ventana del menú para el usuario

---
+++ tallerRetoBladi/FrmMenuP.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tallerRetoBladi
+{
+    public partial class FrmMenuP : Form
+    {
+        public FrmMenuP()
+        {
+            InitializeComponent();
+        }
+    }
+}
